e4a86386e3cab20dd2f1815ec6d506e52dc43e3d
dayeon1211 <dayeon1211m@gmail.com>
2025-05-21 15:27:07 +0900

    JDBC Programming

---
+++ jdbc_ex/jdbc_ex.sql
@@ -0,0 +1,14 @@
+use jdbc_ex;
+
+CREATE TABLE USERS (
+    ID       VARCHAR(12) NOT NULL PRIMARY KEY,
+    PASSWORD VARCHAR(12) NOT NULL,
+    NAME     VARCHAR(30) NOT NULL,
+    ROLE     VARCHAR(6)  NOT NULL
+);
+
+INSERT INTO USERS VALUES ('guest', 'guest123', '방문자', 'USER');
+INSERT INTO USERS VALUES ('admin', 'admin123', '관리자', 'ADMIN');
+INSERT INTO USERS VALUES ('member', 'member1234', '일반회원', 'user');
+
+SELECT * FROM USERS;
