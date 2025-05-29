0e542f44e8e8eed8f3af854f11cee0e4bca9e4a5
dayeon1211 <dayeon1211m@gmail.com>
2025-05-29 09:29:06 +0900

    travelapp jdbc project

---
+++ travelapp/src/main/java/org/scoula/travel/CSVTest1.java
@@ -0,0 +1,14 @@
+package org.scoula.travel;
+
+import java.io.FileReader;
+import com.opencsv.CSVReader;
+
+public class CSVTest1 {
+    public static void main(String[] args) throws Exception {
+        CSVReader csvReader = new CSVReader(new FileReader("travel.csv"));
+        String[] line;
+        while ((line = csvReader.readNext()) != null) {
+            System.out.println(String.join(",", line));
+        }
+    }
+}
